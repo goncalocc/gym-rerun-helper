861e622ed3a807da45550e284ca884e696e74713
goncalocc <goncalo.calducho@gmail.com>
2024-06-26 23:22:53 +0100

    Changes to Backup Restore Teams page

--- src/app/backup/components/BackupRestoreTeams.tsx
+++ src/app/backup/components/BackupRestoreTeams.tsx
@@ -1,15 +1,67 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { Teams, Team } from '../../types/types';
+
+const fetchLocalStorageTeams = (): Teams[] | null => {
+  try {
+    const data = localStorage.getItem('gymRerunTeam');
+    if (!data) {
+      throw new Error('Data not found in localStorage');
+    }
+    return JSON.parse(data);
+  } catch (error: any) {
+    console.error('Error fetching data from localStorage:', error.message);
+    return null;
+  }
+};
+
+const convertStringToObject = (stringData: string[], teamsData: Teams[]) =>{
+  //desconstruir o Teams Data e usar regex para fazer set em cada campo específico
+  return teamsData;
+}
+
+const convertObjectToString = (localStorageData: Teams[]) =>{
+ let stringData
+  const objectData = [...localStorageData];
+ for(let i=0; i < objectData.length; i++) {
+
+ }
+}
 
 const BackupRestoreTeams: React.FC = () => {
-  const [inputValue, setInputValue] = useState<string>('');
+  const [text, setText] = useState<string>('Hello World'); //all teams object in string
+  const [teamsData, setTeamsData] = useState<Teams[]>(); 
+
+  useEffect(() => {
+    const localStorageData = fetchLocalStorageTeams();
+    if (localStorageData) {
+      setTeamsData(localStorageData);
+    }
+    // const stringTeamsData = convertObjectToString(localStorageData);
+  }, []);
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(event.target.value);
+  }
+
+  const handleSave = () => {
+    console.log("teams object gets updated in the string here")
+    //localstorage update
+    // teamsData = convertStringToObject(); 
+    localStorage.setItem('gymRerunTeam', JSON.stringify(teamsData));
+  }
+
+  const handleCancel = () => {
+    setText('');
+  }
 
   return (
     <div className="flex h-screen flex-col p-4">
       <textarea
-        className="w-full flex-grow resize-none rounded border p-2 text-lg"
-        value={inputValue}
+        className="w-full flex-grow resize-none rounded border p-2 text-lg text-black"
+        value={text}
+        onChange={(e) => handleChange(e)}
       />
       <div className="mt-4 flex justify-end space-x-4">
         <button
